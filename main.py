c904a98f86e40794a8ace4200695fbe4cc71d396
TJ Cooper <tjcooper4@gmail.com>
2021-03-15 12:53:54 -0400

    setSpeed function added

--- main.py
+++ main.py
@@ -2,20 +2,26 @@ from algorithms import bubbleSort
 import random
 import time
 
-#def setSpeed(speed):
-    #speed = input((float("Enter a number between 0 and 1"))
+speed = 0
+
+def setSpeed():
+    global speed
+    speed = float(input("Enter a number between 0 and 1: "))
+    
 
 
 
-speed = float(input("Enter a number between 0 and 1: "))
+#speed = float(input("Enter a number between 0 and 1: "))
 
 
-
+print(speed)
 # Test array
 arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
 
 random.shuffle(arr)
 print(arr)
+setSpeed()
+print(speed)
 time.sleep(2)
 bubbleSort(arr, speed)
 
